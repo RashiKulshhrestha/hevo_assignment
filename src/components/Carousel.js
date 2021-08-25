3fed1953afe1180578ce502eada607517b8df21c
Rashi Kulshrestha <rashi@orchids.edu.in>
2021-08-26 00:24:32 +0530

    Added next & prev button on an array

---
+++ src/components/Carousel.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react"
+
+const arr = ["img1", "img2", "img3", "img4"]
+
+export default function Carousel() {
+  const [current, setCurrent] = useState(0)
+  const handlePrev = () => {
+    setCurrent(current === 0 ? arr.length - 1 : current - 1)
+    console.log("prev", current)
+  }
+  const handleNext = () => {
+    setCurrent(current === arr.length - 1 ? 0 : current + 1)
+    console.log("next", current)
+  }
+  return (
+    <div className="carousel-container">
+      <button onClick={handlePrev}>prev</button>
+      <div>
+        {arr.map((item, index) => (
+          <div>{index === current ? <div key={index}>{item}</div> : null}</div>
+        ))}
+      </div>
+      <button onClick={handleNext}>next</button>
+      <div></div>
+    </div>
+  )
+}
